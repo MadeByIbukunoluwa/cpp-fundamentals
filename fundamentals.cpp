636d7e251cd25b3ec1871e5bf12d3ba0d39d65a0
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-04-04 09:04:50 +0100

    feat:modified cpp file added fundamentals

---
+++ fundamentals.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main () {
+    int result = 20 ;
+    std::cout << result << std::endl;
+
+    std::cout << "Number1" << std::endl;
+    std::cout << "Number2" << std::endl;
+    return 0;
+}
+
